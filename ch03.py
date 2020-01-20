c5b502c67e2a466751f0400645a5eb94d994723f
jianglei666 <1317266338@qq.com>
2020-01-20 19:27:51 +0800

    add ch03.py

---
+++ ch03.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 20 17:26:22 2020
+
+@author: Jiang Xinlei
+"""
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+file_path_dau = "C:/Users/Jiang Xinlei/Desktop/Data_Science/Data/section3-dau.csv"
+file_path_dau = "C:/Users/Jiang Xinlei/Desktop/Data_Science/Data/section3-dau.csv"
+file_path_dau = "C:/Users/Jiang Xinlei/Desktop/Data_Science/Data/section3-dau.csv"
+dau = pd.read_csv(file_path, sep=',')
